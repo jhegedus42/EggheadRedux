693a2f625c23a84f87cbc4d48c8e3d5af569c755
Cintia Higashi <cintiamh@gmail.com>
2016-07-06 09:45:00 -0700

    Adding a new todo working

--- src/todo.js
+++ src/todo.js
@@ -15,10 +15,9 @@ const todo = (state, action) => {
       if (todo.id !== action.id) {
         return state;
       }
-      return {
-        ...state,
+      return Object.assign({}, state, {
         completed: !state.completed
-      }
+      });
     default:
       return state;
   }
@@ -61,3 +60,47 @@ const todoApp = combineReducers({
 });
 
 const store = createStore(todoApp);
+
+let nextTodoId = 0;
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref={node => {
+          this.input = node;
+        }} />
+        <button onClick={() => {
+          store.dispatch({
+            type: 'ADD_TODO',
+            text: this.input.value,
+            id: nextTodoId++
+          });
+          this.input.value = '';
+        }}>
+          Add Todo
+        </button>
+        <ul>
+          {
+            this.props.todos.map(todo =>
+              <li key={todo.id}>
+                {todo.text}
+              </li>
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(
+    <TodoApp todos={store.getState().todos} />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+window.onload = () => {
+  render();
+}
