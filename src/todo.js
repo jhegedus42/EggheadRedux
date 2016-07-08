0cd24876978c13e9ef7366f37874d64ba4ee5dcf
Cintia Higashi <cintiamh@gmail.com>
2016-07-07 17:16:14 -0700

    Extracting components: Todo and TodoList

--- src/todo.js
+++ src/todo.js
@@ -80,6 +80,30 @@ const FilterLink = ({ filter, currentFilter, children }) => {
     )
 }
 
+const TodoComponent = ({ onClick, completed, text }) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration:
+        completed ? 'line-through' : 'none'
+    }}
+  >
+    {text}
+  </li>
+)
+
+const TodoList = ({todos, onTodoClick}) => (
+  <ul>
+    {todos.map(todo =>
+      <TodoComponent
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
+
 const getVisibleTodos = (todos, f) => {
     switch (f) {
         case 'SHOW_ALL':
@@ -111,26 +135,15 @@ class TodoApp extends React.Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {
-            visibleTodos.map(todo =>
-              <li key={todo.id}
-                onClick={() => {
-                  store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id: todo.id
-                  });
-                }}
-                style={{
-                  textDecoration:
-                    todo.completed ? 'line-through' : 'none'
-                }}
-              >
-                {todo.text}
-              </li>
-            )
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
           }
-        </ul>
+        />
         <p>
             Show:
             {' '}
